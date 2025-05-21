7ef2eb59652ba3e424237197197d4ade4b23c824
Keitaro Nakamrua <numerugon487@gmail.com>
2025-05-21 17:30:19 +0900

    update sentry_ln.launch

--- launch/sentry_lidarlocalization_nav2.launch.py
+++ launch/sentry_lidarlocalization_nav2.launch.py
@@ -27,59 +27,45 @@ def generate_launch_description():
 #            remappings=[('/scan','/ldlidar_node/scan'),('/scan_filtered','/scan')],
 #    )
 
-    tf2_base_link_lidar_base = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0.13', '0', '0.6', '1', '0', '0', '0', 'base_link', 'lidar_base']
+#    tf2_base_link_lidar_base = Node(
+#        package='tf2_ros',
+#        executable='static_transform_publisher',
+#        arguments=['0.13', '0', '0.6', '1', '0', '0', '0', 'base_link', 'lidar_base']
+#    )
+
+    exec_lidar_localization = ExecuteProcess(
+        cmd=[ 'ros2', 'launch',
+            'lidar_localization_ros2 ', 'lidar_localization.launch.py'],
+        output='screen',
     )
 
-    bringup_dir = get_package_share_directory('turtlebot3_cartographer')
-    cartographer_config_dir = os.path.join(bringup_dir, 'config')
-    cartographer_map_dir = os.path.join(bringup_dir, 'map')
-    configuration_basename = 'noimu_lds_2d_nav.lua'
-
-    use_sim_time = False
-    resolution = '0.05'
-    publish_period_sec = '1.0'
-
-    cartographer_node = Node(
-            package='cartographer_ros',
-            executable='cartographer_node',
-            name='cartographer_node',
+    convert_pose2tf_node = Node(
+            package='convert_pose2tf',
+            executable='convert_pose2tf',
+            name='convert_pose2tf_node',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-configuration_directory', cartographer_config_dir,
-                       '-configuration_basename', configuration_basename]
     )
-
-    occupancy_grid_node = Node(
-            package='cartographer_ros',
-            executable='cartographer_occupancy_grid_node',
-            name='cartographer_occupancy_grid_node',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]
-    )
+            
     
-    nav2_config = os.path.join(cartographer_config_dir, 'nav2_param.yaml')
-    nav2_map = os.path.join(cartographer_map_dir, 'map.yaml')
-
-    nav2_launch = IncludeLaunchDescription(
-        launch_description_source=PythonLaunchDescriptionSource([
-            get_package_share_directory('nav2_bringup'),
-            '/launch/bringup_launch.py'
-        ]),
-        launch_arguments={
-                'use_sim_time': 'False',
-                'params_file': nav2_config,
-                'map': nav2_map,
-        }.items()
-    )
+#    nav2_config = os.path.join(cartographer_config_dir, 'nav2_param.yaml')
+#    nav2_map = os.path.join(cartographer_map_dir, 'map.yaml')
+#
+#    nav2_launch = IncludeLaunchDescription(
+#        launch_description_source=PythonLaunchDescriptionSource([
+#            get_package_share_directory('nav2_bringup'),
+#            '/launch/bringup_launch.py'
+#        ]),
+#        launch_arguments={
+#                'use_sim_time': 'False',
+#                'params_file': nav2_config,
+#                'map': nav2_map,
+#        }.items()
+#    )
 
     rviz2_config = os.path.join(
-        get_package_share_directory('turtlebot3_cartographer'),
+        get_package_share_directory('sentry_master'),
         'rviz',
-        'tb3_cartographer_nav.rviz'
+        'sentry_cn.rviz'
     )
 
     rviz2_node = Node(
@@ -92,12 +78,12 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
-#    ld.add_action(ldlidar_launch)
-#    ld.add_action(laser_filter_node)
-    ld.add_action(tf2_base_link_lidar_base)
-    ld.add_action(cartographer_node)
-    ld.add_action(occupancy_grid_node)
-    ld.add_action(nav2_launch)
+#    ld.add_action(tf2_base_link_lidar_base)
+#    ld.add_action(cartographer_node)
+#    ld.add_action(occupancy_grid_node)
+    ld.add_action(exec_lidar_localization)
+    ld.add_action(convert_pose2tf_node)
+#    ld.add_action(nav2_launch)
     ld.add_action(rviz2_node)
     return ld
 
